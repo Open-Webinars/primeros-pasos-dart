c77dec2acbb99c55a6d83279f6340159c9435c3d
jose09511 <jose09511@gmail.com>
2022-01-03 11:01:52 +0100

    variable string

---
+++ 03_variables/ejercicios.dart
@@ -0,0 +1,24 @@
+/*
+EJERCICIO 1
+Imprime la última letra del nombre "Sánchez"
+
+void main() {
+  String nombre = "Sánchez";
+  print("${nombre[nombre.length - 1]}");
+}
+
+EJERCICIO 2
+Sustituye en la frase todos los String "Jose" por u String "Maria" utilizándolos
+como variables independientes
+
+String frase = "Jose hola como estas Jose si que estás bien Jose no se cuantas veces decir Jose, ¿Eh Jose?"
+void main() {
+  String nombre = "Jose";
+  String nombre2 = "Maria";
+
+  print(frase.replaceAll(nombre, nombre2));
+}
+
+*/
+
+
